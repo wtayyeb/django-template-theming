05aae518f4de9c5a731bd6d3417ee93820c58466
Tayyeb <tayyeb@tsaze.com>
2015-11-01 03:08:38 +0330

    add favicon redirect view

--- theming/views.py
+++ theming/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# -*- coding:utf-8 -*-
+'''
+@author: wTayyeb  https://github.com/wtayyeb
+@license: MIT
+'''
 
-# Create your views here.
+import os
+
+from django.conf import settings
+from django.http.response import HttpResponseRedirect
+
+from .models import thememanager
+from django.templatetags.static import static
+
+
+def redirect_to_theme_fav_icon(request):
+    theme = thememanager.get_current_theme()
+    url = static(os.path.join(settings.THEMING_URL, theme.slug, '/images/favicon.ico'))
+    return HttpResponseRedirect(url)
