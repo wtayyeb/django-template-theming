1d1d444164b4e342618aafc51b182f3e9530f277
Tayyeb <tayyeb@tsaze.com>
2015-11-01 00:16:13 +0330

    initial migration for theming

---
+++ theming/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('sites', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='SiteTheme',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('theme_slug', models.CharField(max_length=100, choices=[(b'default', b'Default'), (b'responsive', 'Simple Responsive Theme'), (b'lapic', b'Lapic'), (b'vecto', b'Vecto')])),
+                ('site', models.OneToOneField(to='sites.Site')),
+            ],
+        ),
+    ]
