18b4c15df247f2da36ed7a51ea01ae6daa8ae473
Tayyeb <tayyeb@tsaze.com>
2015-11-01 01:42:15 +0330

    improve example project templates

--- example/simpleapp/views.py
+++ example/simpleapp/views.py
@@ -1,7 +1,10 @@
 
 from django.shortcuts import render_to_response
 
+from theming.models import thememanager
+
 
 def index(request):
+    print 'rendering with', thememanager.get_current_theme()
     return render_to_response('simpleapp_index.html')
 
