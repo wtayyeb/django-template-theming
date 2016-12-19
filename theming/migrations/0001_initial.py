91062729f10c406c1b2ff73a9ba4bfe1002f3e40
Tayyeb <tayyeb@tsaze.com>
2016-12-19 16:28:51 +0330

    fix theming fake migrate creation

--- theming/migrations/0001_initial.py
+++ theming/migrations/0001_initial.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 
+from theming.models import thememanager
+
 
 class Migration(migrations.Migration):
 
@@ -15,7 +17,7 @@ class Migration(migrations.Migration):
             name='SiteTheme',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('theme_slug', models.CharField(max_length=100, choices=[(b'default', b'Default'), ])),
+                ('theme_slug', models.CharField(max_length=100, choices=thememanager.get_themes_choice())),
                 ('site', models.OneToOneField(to='sites.Site')),
             ],
         ),
