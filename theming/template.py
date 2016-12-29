ebd4cf74384f9385c3305133afce8a7a097b3186
Tayyeb <tayyeb@tsaze.com>
2016-12-30 00:55:08 +0330

    theming Loader will only pass selected theme files

--- theming/template.py
+++ theming/template.py
@@ -39,34 +39,29 @@ class Loader(BaseLoader):
         theme = thememanager.get_current_theme()
 
         if not template_dirs:
-            template_dirs = [settings.THEMING_ROOT, ]
+            template_dirs = [safe_join(settings.THEMING_ROOT, theme.slug), ]
 
         for template_dir in template_dirs:
             try:
-                yield safe_join(template_dir, theme.slug, template_name)
-            except UnicodeDecodeError:
-                # The template dir name was a bytestring that wasn't valid UTF-8.
-                raise
+                yield safe_join(template_dir, template_name)
             except SuspiciousFileOperation:
-                # The joined path was located outside of this particular
-                # template_dir (it might be inside another one, so this isn't
-                # fatal).
+                # The joined path was located outside of this template_dir
+                # (it might be inside another one, so this isn't fatal).
                 pass
-
 
     def load_template_source(self, template_name, template_dirs=None):
         tried = []
         for filepath in self.get_template_sources(template_name, template_dirs):
             try:
-                encoding = self.engine.file_charset if hasattr(self, 'engine') and self.engine else 'utf-8'
-                with io.open(filepath, encoding=encoding) as fp:
+                with io.open(filepath, encoding=self.engine.file_charset) as fp:
                     return fp.read(), filepath
             except IOError:
                 tried.append(filepath)
         if tried:
             error_msg = "Tried %s" % tried
         else:
-            error_msg = "Your TEMPLATE_DIRS setting is empty. Change it to point to at least one template directory."
+            error_msg = ("Your template directories configuration is empty. "
+                         "Change it to point to at least one template directory.")
         raise TemplateDoesNotExist(error_msg)
     load_template_source.is_usable = True
 
@@ -78,6 +73,7 @@ def context_processor(request):
     theme_url = static(os.path.join(settings.THEMING_URL, theme.slug)).replace('\\', '/')
     return {
         'theme_url': theme_url,
+        'sitetheme': theme,
     }
 
 
